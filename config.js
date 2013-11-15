a47f471cee59d086e0a99143ff5600b0e09c2920
André Ligné <andre.ligne@gmail.com>
2013-11-15 14:44:51 +0100

    config.js now uses relative paths instead of absolute. server.sh also calls 'python' instead of 'python2'

--- config.js
+++ config.js
@@ -1,43 +1,43 @@
 var testsData = [
     {
 		name: "Dromaeo", 
-		path: "/adapter/dromaeo/",
+		path: "adapter/dromaeo/",
 		version: "0.1",
 		group: ["JS"]
     },
     {
 		name: "Testing", 
-		path: "/adapter/testing/",
+		path: "adapter/testing/",
 		version: "0.1",
 		group: ["JS"]
     },
     {
 		name: "Testing2", 
-		path: "/adapter/testing2/",
+		path: "adapter/testing2/",
 		version: "0.1",
 		group: [""]
     },
     {
 		name: "Sunspider", 
-		path: "/adapter/sunspider/",
+		path: "adapter/sunspider/",
 		version: "1.0.1",
 		group: [""]
     },
     {
 		name: "themaninblue-svg",
-		path: "/adapter/themaninblue-svg/",
+		path: "adapter/themaninblue-svg/",
 		version: "0.1",
 		group: [""]
     },
     {
 		name: "themaninblue-canvas",
-		path: "/adapter/themaninblue-canvas/",
+		path: "adapter/themaninblue-canvas/",
 		version: "0.1",
 		group: [""]
     },
     {
 		name: "themaninblue-html",
-		path: "/adapter/themaninblue-html/",
+		path: "adapter/themaninblue-html/",
 		version: "0.1",
 		group: [""]
     }    
--- server.sh
+++ server.sh
@@ -1,1 +1,1 @@
-python2 -m SimpleHTTPServer
+python -m SimpleHTTPServer
