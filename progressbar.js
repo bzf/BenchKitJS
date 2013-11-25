1ec06eb53f4c00d37b6789678d06f421551b7ae2
Bjornmagnus <jonathan@elektronvolt.se>
2013-11-25 15:35:24 +0100

    Yes, I was being sarcastic when I told you I liked global variables. Also modifies the progressbar to be css.

--- progressbar.js
+++ progressbar.js
@@ -6,65 +6,41 @@
 
 var Progressbar = function createProgressbar(parent_id, name, size)
 {
-	var PROGRESSBAR_SIZE = 57;
 	var clear = document.createElement("div");
 	clear.setAttribute("class", "clear");
 
 	/* Define the updateBar-function */
 	var update = function() {
-		var done = Math.floor((current / size) * PROGRESSBAR_SIZE);
-		var progress = new Array(done).join("#");
-		progress += new Array(PROGRESSBAR_SIZE - done + 1).join("-");
-		progress = "[" + progress + "]";
-
-		html_bar.innerHTML = progress;
+		progressbar.children[0].children[1].children[0].style["width"] = current + "%"
 	};
 
 	/* Data worth holding onto */
-	name = name;
-	size = size;
-	current = 0;
-	current_test = "Nothing";
+
+	var current = 0;
+	var current_test = "Nothing";
 
 	/* Create the progressbar */
-	progressbar = document.createElement("div");
-	progressbar.setAttribute("class", "progressbar");
+	var progressbar = document.createElement("div");
+	progressbar.setAttribute("id", name);
+	progressbar.innerHTML = "<div><span class='bar-title'>" + name +"</span><div class='progressbar'><div class='bar'></div></div></div>"
 
-	/* Create and append content to the progressbar */
-	html_name = document.createElement("p");
-	html_name.innerHTML = name;
-	progressbar.appendChild(html_name);
-
-	running_test = document.createElement("p");
-	running_test.innerHTML = "* Running: " + current_test;
-	progressbar.appendChild(running_test);
-
-	html_bar = document.createElement("p");
-	html_bar.setAttribute("class", "bar");
-	progressbar.appendChild(html_bar);
-	update();
 
 	/* Add the progressbar to the parent */
 	var parent_div = document.getElementById(parent_id);
 	parent_div.appendChild(progressbar);
 	parent_div.appendChild(clear);
 
+	update()
+		
 	/* Return the tick-function */
 	return function(name_of_test) {
 		/* Tick and set the name of next test */
-		current += 1;
-		current_test = (current == size) ? "Done" : name_of_test;
+		current += (1/size)*100;
 
-		/* Update the progressbar */
-		running_test.innerHTML = "* Running: " + current_test
-			+ "(" + current + "/" + size + ")";
 		update();
+		progressbar.children[0].children[0].innerHTML = name + name_of_test
 
-		return (current == size);
+		return (current >= 100);
 	};
 };
 
-var tick_first = new Progressbar("pbc", "Ossler", 10);
-t1 = setInterval(function() {
-	if (tick_first("hej")) clearInterval(t1);
-}, 100);
