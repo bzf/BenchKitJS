0f63f4dff6b0a9dd8d489f4d1ae1ea5a50b0acfd
stiggan <stiggan@bloodphoenix.net>
2013-12-10 13:20:37 +0100

    Added removeGroup to remove a whole group of tests. Added support for argument remove_group which takes a list of group names which it will delete from the result.

--- adapter/html5test/adapter.js
+++ adapter/html5test/adapter.js
@@ -10,27 +10,28 @@
  *
  */
 
-var Adapter = function() {
+
+var Adapter = function(args) {
     this.config = {
         name : "HTML5test",
         version : "5.0"
     };
-
-    this.runTest();
+    this.args = args
+    this.runTest()
 }
 
 Adapter.prototype.runTest = function() {
-    var self = this;
+    var self = this
     
-    document.getElementById("adapterFrame").src = "test.html";
-    
+    document.getElementById("adapterFrame").src = "test.html"
+
     var iframe = document.getElementById("adapterFrame");
-    var contentWindow = iframe.contentWindow;
+    var contentWindow = iframe.contentWindow
 
     iframe.onload = function() {
         //Overwrite the finish function in test.html.
         contentWindow.finish = function(data) {
-            self.parseData(data);
+            self.parseData(data)
 	   }
     }
 
@@ -45,6 +46,29 @@ function removeResult(result, group, variable, score) {
     var group_result = result["points"][group].split("/")
     result["points"][group] = group_result[0] + "/" + (group_result[1]-score) 
 
+    return result
+}
+
+function removeGroup(result, group) {
+    /*
+     * If there is no startsWith function in the string object, we create one
+     * since.... well it's very neat to have.
+     */
+    if (typeof String.prototype.startsWith != 'function') {
+        String.prototype.startsWith = function (str){
+            return this.slice(0, str.length) == str;
+        };
+    }
+    for var key, var value in result["result"] {
+        if (key.startsWith(group+"-")) {
+            removeResult(result, group, key.slice(group.length+1, key.length), 0);
+        }
+    }
+    var group_result = result["points"][group].split("/")
+    result["maximum"] -= group_result[1]
+    result["score"] -= group_result[0]
+    //result["points"][group] = group_result[0] + "/" + (group_result[1]-score)
+    delete result["points"][group] 
     return result
 }
 
@@ -68,20 +92,25 @@ Adapter.prototype.parseData = function(data) {
      * Removing csp10 and csp11 since these tests can't be run on a python
      * SimpleHTTP webserver.
      */
-    result = removeResult(result, "security", "csp10", 3);
-    result = removeResult(result, "security", "csp11", 2);
+    result = removeResult(result, "security", "csp10", 3)
+    result = removeResult(result, "security", "csp11", 2)
 
     /*
      * Removing indexedDB.arraybuffer since this one randomly show true or false
      * even when it should show false.
      */
-    result = removeResult(result, "storage", "indexedDB.arraybuffer", 2);
+    result = removeResult(result, "storage", "indexedDB.arraybuffer", 2)
+
+    if (args["remove_group"])
+        for (var group_name in args["remove_group"]) {
+            removeGroup(result, group_name)
+        }
 
     window.parent.output("- Result: " + result["score"] + "/" + result["maximum"] + " - ", "output-" + this.config.name, false)
 
-    window.parent.adapterDone(result);
+    window.parent.adapterDone(result)
 }
     
 function createAdapter(args) {
-    new Adapter()
+    new Adapter(args)
 }
